0cfb2654a046326bbfd097af081e614c3b7b99c8
ASUS CORE i5 <samuelfaka@gmail.com>
2024-07-29 02:50:16 -1100

    first commit

--- index.js
+++ index.js
@@ -1,69 +1,36 @@
-// const clickme = () => {
-//     const main = document.querySelector(".box");
-//     const input = document.querySelector(".in2").value;
-//     const box = document.querySelector(".box");
-//     box.style.display ="block"
-    
-//     if (!input) {
-//         return alert('Enter something');
-//     }
-    
-//     const liElement = document.createElement("li");
-//     liElement.textContent = input;
-//     liElement.style.color = "black";
-//     liElement.style.fontSize = "15px";
-//     liElement.style.gap = "20px"
-//     liElement.style.marginBottom = "5px";
-    
-//     const hrElement = document.createElement("hr");
-//     hrElement.style.width = "24vw";
-    
-//     let ulElement = document.querySelector(".box ul");
-//     if (!ulElement) {
-//         ulElement = document.createElement("ul");
-//         document.querySelector(".box").appendChild(ulElement);
-//     }
-    
-//     ulElement.appendChild(liElement);
-//     ulElement.appendChild(hrElement);
-//     main.appendChild(ulElement)
-    
-//     document.querySelector(".in2").value = '';
-    
-//     document.querySelector(".box").style.display = "flex";
-// };
+const sunIcon = document.querySelector(".change");
+const moonIcon = document.querySelector(".change1");
 
-const change = document.querySelector(".change");
-const sec = document.querySelector(".sec");
-const box = document.querySelector(".box");
-const both = document.querySelector(".both");
-const btn = document.querySelector(".btn");
-const user1 = document.querySelector(".user1")
+let isDarkMode = false;
 
-let isBlack = false;
-
-change.addEventListener("click", function () {
-    if (isBlack) {
-        user1.style.background = "white";
-        sec.style.background = "white";
-        box.style.background = "white";
-        both.style.background = "white";
-        btn.style.background = "white";
+function toggleTheme() {
+    if (isDarkMode) {
+        document.documentElement.style.setProperty('--background-light', 'white');
+        document.documentElement.style.setProperty('--background-dark', 'white');
+        document.documentElement.style.setProperty('--btn-color', 'white');
+        document.documentElement.style.setProperty('--box-background', 'white');
+        sunIcon.style.display = 'none';
+        moonIcon.style.display = 'inline';
     } else {
-        user1.style.background = "black";
-        sec.style.background = "black";
-        box.style.background = "black";
-        both.style.background = "black";
-        btn.style.background = "black";
+        document.documentElement.style.setProperty('--background-light', 'black');
+        document.documentElement.style.setProperty('--background-dark', 'black');
+        document.documentElement.style.setProperty('--btn-color', 'black');
+        document.documentElement.style.setProperty('--box-background', 'black');
+        sunIcon.style.display = 'inline';
+        moonIcon.style.display = 'none';
     }
-    isBlack = !isBlack;
-});
+    isDarkMode = !isDarkMode;
+}
+
+sunIcon.addEventListener("click", toggleTheme);
+moonIcon.addEventListener("click", toggleTheme);
+
 
 const clickme = () => {
     const main = document.querySelector(".box");
 
     const check = document.createElement("input");
-    check.setAttribute("type", "radio");
+    check.setAttribute("type", "checkbox");
 
     const hrElement = document.createElement("hr");
 
